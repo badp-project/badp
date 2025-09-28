adc3d975b0d04a76a1bad2cb83df4f23f347ba88
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2025-09-28 13:33:10 +0200

    test: increase params tolerance to 0.03

--- tests/testthat/test-model_space.R
+++ tests/testthat/test-model_space.R
@@ -186,7 +186,7 @@ test_that("Moral-Benito BMA results are replicated (main branch only)", {
   actual <- bma_results[[1]]
   expected <- bdsm::full_bma_results[[1]]
 
-  ms_params_tols <- rep(0.02, ncol(full_model_space$params))
+  ms_params_tols <- rep(0.03, ncol(full_model_space$params))
   ms_stats_tols <- rep(0.02, ncol(full_model_space$stats))
 
   compare_matrices(model_space$params, full_model_space$params, ms_params_tols)
