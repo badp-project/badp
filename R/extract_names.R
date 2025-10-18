5ae0da7dcd1f1c4ed3bc61182e7a5a63bd08fd39
becku182 <beckkrzysztof@gmail.com>
2025-10-18 12:01:23 +0200

    This comm it removes the requirement of using df in bma function - it makes appropriate ch anges in all places

---
+++ R/extract_names.R
@@ -0,0 +1,24 @@
+#' Extraction of names of the variables
+#'
+#' The function extract the names of the variables from the data set used in the analysis and
+#' places them in a vector.
+#'
+#'
+#' @param df Data frame with data for the analysis.
+#'
+#' @return A vector with names of the variables.
+#'
+#' @examples
+#' \donttest{
+#'  df <- bdsm::economic_growth
+#'
+#'  reg_names <- extract_names(df)
+#'
+#' }
+extract_names <- function(df){
+  reg_names <- colnames(df)
+  reg_names <- reg_names[-(1:2)]
+  reg_names[1] <- paste0(reg_names[1], "_lag")
+
+  return(reg_names)
+}
