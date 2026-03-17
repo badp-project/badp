88e62b4a52265759783145f50e189600508571fd
Marcin Dubel <marcin@appsilon.com>
2026-03-17 22:57:39 +0100

    Remove ggpubr dependency and update plotting functions to use custom arrangement

--- R/model_pmp.R
+++ R/model_pmp.R
@@ -95,13 +95,13 @@ Graph1 <- ggplot2::ggplot(forGraph1, ggplot2::aes(x = ID, y = Value)) +
   ggplot2::geom_line(ggplot2::aes(color = Probability, linetype = Probability)) +
   ggplot2::scale_color_manual(values = c("darkred", "steelblue")) +
   ggplot2::ylab("Prior, Posterior") +
-  ggplot2::xlab("Model number in the raniking")
+  ggplot2::xlab("Model number in the ranking")
 
 Graph2 <- ggplot2::ggplot(forGraph2, ggplot2::aes(x = ID, y = Value)) +
   ggplot2::geom_line(ggplot2::aes(color = Probability, linetype = Probability)) +
   ggplot2::scale_color_manual(values = c("darkred", "steelblue")) +
   ggplot2::ylab("Prior, Posterior") +
-  ggplot2::xlab("Model number in the raniking")
+  ggplot2::xlab("Model number in the ranking")
 
 if (dilution==0){
 Graph1_2 <- ggplot2::ggplot(forGraph1, ggplot2::aes(x = ID, y = Value)) +
@@ -136,9 +136,7 @@ if (dilution==1){
 }
 
 # Putting together the last plot
-Finalplot <- ggpubr::ggarrange(Graph1_2,Graph2_2,
-                               labels = c("a)", "b)"),
-                               ncol = 1, nrow = 2, common.legend = TRUE, legend = "bottom")
+Finalplot <- arrange_plots_common_legend(Graph1_2, Graph2_2)
 
 print(Finalplot)
 
