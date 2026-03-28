a08637179f78173bf31ecbd146221cc8f459e462
becku182 <beckkrzysztof@gmail.com>
2026-03-28 22:45:45 +0100

    add migration dataset and model_space object

--- R/data.R
+++ R/data.R
@@ -155,3 +155,58 @@
 #'
 #' A list with multiple elements summarising the BMA analysis
 "full_bma_results"
+
+
+#' Migration data in the original format
+#'
+#' Data used in the manuscript Beck, K., Dubel, M., Wyszyński, M., & Yersh, V. (2025).
+#' Most seek higher earnings, some escape unemployment, but none look for a handout:
+#' Economic drivers of labor mobility in the European Union.
+#' https://doi.org/10.2139/ssrn.5472939
+#'
+#' @format ## `migration_data`
+#' A data frame with 1012 rows and 8 columns
+#' (253 country pairs and 4 periods + one additional observation for the lagged dependent variable):
+#' \describe{
+#'   \item{Time}{Year}
+#'   \item{Pair}{Country pair ID}
+#'   \item{Mig}{Net migration between two countries}
+#'   \item{Mig_lag}{Lagged net migration between two countries}
+#'   \item{Earn}{Difference in average real after-tax earnings in PPP}
+#'   \item{Unemp}{Difference in the unemployment rate}
+#'   \item{Social}{Difference in average social benefits in PPP}
+#'   \item{Tax}{Difference in average tax rate}
+#' }
+#' @source <https://doi.org/10.7910/DVN/M9DADS>
+"migration_data"
+
+#' Example output of \code{\link{optim_model_space}} in the case of migration data
+#'
+#' A list created with \code{\link{optim_model_space}} using the
+#' \code{\link{migration_data}} dataset.
+#'
+#' @format A list with 5 elements:
+#' \describe{
+#'   \item{params}{
+#'     A numeric matrix with 51 rows and 16 columns, containing parameter
+#'     values for the full model space. Each column represents a different model.
+#'   }
+#'   \item{stats}{
+#'     A numeric matrix of statistics computed by
+#'     \code{\link{compute_model_space_stats}} based on \code{params}.
+#'     Row 1 contains model likelihoods. Row 2 contains a quantity proportional
+#'     to \code{0.5 * BIC} (cf. Raftery, Bayesian Model Selection in Social Research,
+#'     Eq. 19). Rows 3--7 contain standard deviations, and rows 8--12 contain
+#'     robust standard deviations.
+#'   }
+#'   \item{reg_names}{
+#'     A character vector with the names of the variables.
+#'   }
+#'   \item{observations_num}{
+#'     The total number of observations in the panel (1012).
+#'   }
+#'   \item{df}{
+#'     The data frame used in the analysis.
+#'   }
+#' }
+"migration_model_space"
