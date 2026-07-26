ad07d49b318fadfdc281bc087c149195ba1fe9fe
Mateusz Wyszynski <mat.wyszynski@gmail.com>
2026-07-26 08:57:32 +0200

    feat: always use a generator for init_value and regenerate at init if in infeasible region

--- data-raw/full_model_space.R
+++ data-raw/full_model_space.R
@@ -29,7 +29,7 @@ full_model_space <- optim_model_space(
   dep_var_col   = gdp,
   timestamp_col = year,
   entity_col    = country,
-  init_value    = 0.5,
+  init_value    = function(n) rep(0.5, n),
   cl = cl
 )
 
